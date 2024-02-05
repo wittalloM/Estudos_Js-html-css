51f89ae6b4688caa47d60b2387ce94c1af61634b
wittalloM <wittallo@edu.uniube.br>
2024-02-05 11:55:42 -0300

    Adição PS1

---
+++ PS-1/Scripts/ps-Script.js
@@ -0,0 +1,13 @@
+
+
+
+
+const card1_ht = document.getElementById("card1-back")
+const card1_pai = document.getElementsByClassName("card1")
+const usr_Name = document.getElementsByClassName(".usr-name")
+
+card1_pai.addEventListener("click", () => { console.log("Está Funcionando") });
+usr_Name.addEventListener("click", () => {
+    return console.log("Está Funcionando");
+});
+
